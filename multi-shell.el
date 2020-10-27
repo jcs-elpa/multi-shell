d33f0177ae81be5184155b454cfc758c6b5cbd34
Jen-Chieh <jcs090218@gmail.com>
2020-10-27 12:50:28 +0800

    Bump version.'

--- multi-shell.el
+++ multi-shell.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Managing multiple shell buffers.
 ;; Keyword: multiple shell terminal
-;; Version: 0.0.5
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/jcs-elpa/multi-shell
 
